0345effa3e4ef1b36d4682f10f0edf822735c542
WinstonMuijs <56136891+WinstonMuijs@users.noreply.github.com>
2023-08-27 14:13:48 +0200

    Update controllers

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -5,42 +5,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApi.Controllers
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    // GET: api/Users
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        // GET: api/Users
-        [HttpGet]
-        public IEnumerable<string> Get()
+        List<string> output = new();
+        for(int i = 1; i < Random.Shared.Next(2,20); i++)
         {
-            return new string[] { "value1", "value2" };
+            output.Add($" Value #{i}");
         }
+        return output;
+    }
 
-        // GET: api/Users/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // GET: api/Users/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id)
+    {
+        return $"{id + 10}";
+    }
 
-        // POST: api/Users
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    // POST: api/Users
+    [HttpPost]
+    public void Post([FromBody] string value)
+    { 
+    }
 
-        // PUT: api/Users/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    // PUT: api/Users/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
 
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE: api/Users/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
