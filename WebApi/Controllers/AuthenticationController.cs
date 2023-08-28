a34bd8a9fa5533c65c66f8a8912a70c0ed9f0dac
WinstonMuijs <56136891+WinstonMuijs@users.noreply.github.com>
2023-08-28 19:57:19 +0200

    added Comments
    For a better understanding

--- WebApi/Controllers/AuthenticationController.cs
+++ WebApi/Controllers/AuthenticationController.cs
@@ -4,9 +4,18 @@ namespace WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+/* Authenticatie Cotroller:
+ * Wie ben jij? 
+ * En ben jij wie je zegt dat je bent.
+ */
 public class AuthenticationController : ControllerBase
+
 {
+    /* Een record om informatie vast te leggen over een gebruiker.
+     * inlog gegevens zoals naam en paswoord.
+    */
     public record AuthenticationData(string? UserName, string? Password);
+    /* Als Authenticatie data klopt krijg je UserData terug */
     public record UserData(int UserId, string UserName);
 
     // api/Authentication/token
@@ -15,10 +24,16 @@ public class AuthenticationController : ControllerBase
     {
         
     }
-
+    /* Validatie process: 
+     * door derde partij Azure Active Directory of Auth0
+     * ingevoerde gegevens worden vergeleken met de verwachte waarden.
+     * Als ze overeenkomen geef gebruiker Id en Naam, ander Null.
+     */
     private UserData? ValidateCredentials(AuthenticationData data)
     {
-        //valideren van onze data door derde partij authenticatie provider in productie
+        /*valideren van onze data door derde partij authenticatie provider in productie
+         * In deze Demo setting zo hardcoded.
+        */
         if(CompareValues(data.UserName, "whmuijs") &&
             CompareValues(data.Password, "Test123"))
         {
@@ -27,7 +42,9 @@ public class AuthenticationController : ControllerBase
 
         return null;
     }
-
+    /*Methode of functie over de invoer waarden met de verwachte waarden tevergelijken.
+     * Als ze ovvereenkomen geef true anders false.
+     */
     private bool CompareValues(string? actual, string expected)
     {
         if(actual is not null)
