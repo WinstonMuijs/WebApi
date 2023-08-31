b1e2f11947db122cd820b72487b68eb930770035
WinstonMuijs <56136891+WinstonMuijs@users.noreply.github.com>
2023-08-31 14:14:30 +0200

    Update

--- WebApi/Constants/Policies.cs
+++ WebApi/Constants/Policies.cs
@@ -3,7 +3,8 @@ namespace WebApi.Constants
 {
 	public static class Policies
 	{
-		public const string MustHaveStudentsId = "MustHaveStudentId";
+
+        public const string MustHaveStudentsId = "MustHaveStudentId";
         public const string MajorStudent = "MajorStudent";
         public const string YearOfClass = "YearOfClass";
         public const string Title = "Master";
